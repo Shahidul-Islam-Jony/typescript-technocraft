7ca2d3e78e288f08079ec472f609b4466a0ae7a1
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-04 11:45:15 +0600

    M-1-4 Basic data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,40 @@
+// Basic data type
+
+// string
+
+let firstName : string = "Rodix"
+
+// number
+let roll : number = 143
+
+// boolean
+let isAdmin : boolean = true
+
+// undefined
+let x: undefined = undefined
+
+// null
+let y: null = null
+
+
+// 
+let d:number;
+// d='hello'; //error dibe
+d=123;
+
+// Array
+
+let friends:string[] = ['rachel','monica']
+
+// friends.push(7) // number push kora jave na. string e push korte hobe.
+
+
+let eligibleRollList : number[] = [1,2]
+
+
+// tuple ---> array --->order --> type of values
+
+let oordinates:[number,number] =[1,5]
+
+let ageName : [number,string,boolean] = [50,'Mr.X',true]
+
