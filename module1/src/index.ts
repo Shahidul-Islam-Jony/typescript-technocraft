9d5c6cb68949c2336b7cff909e96b882db34b444
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-04 11:23:10 +0600

    Set up typescript configration file m 1-3

---
+++ module1/src/index.ts
@@ -0,0 +1,6 @@
+// typescript configration file install korte hole tsc --init dite hobe
+// tsconfig.ts file a issa moto configration kore newa jabe module 1-3
+
+
+let course: string = 'Next level web development';
+console.log(course);
