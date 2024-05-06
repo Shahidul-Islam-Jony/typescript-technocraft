155073bde6bf36aa9a060c54e8e1cd27f4266ced
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-05-06 10:26:57 +0600

    M(2-1) Type Assertion / type narrowing

---
+++ module2/2.1.ts
@@ -0,0 +1,42 @@
+{
+    // 
+
+    // Type assertion
+    let anything: any;
+
+    anything = "Next Level Web Development";
+    anything = 222;
+
+    // (anything as string).    // (anything as typeName) diye . dilei auto suggesition dibe , eitai type assertion
+    // (anything as number).
+
+
+    const kgToGm = (value: string | number): string | number | undefined => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The converted value is : ${convertedValue}`;
+        }
+        if (typeof value === 'number') {
+            return value * 1000;
+        }
+    }
+
+    const result1 = kgToGm(1000) as number;
+    const result2 = kgToGm("1000") as string;
+
+
+    type CustomError = {
+        message:string
+    }
+
+    try{
+
+    }
+    catch(error){
+        console.log((error as CustomError).message);
+    }
+
+
+
+    // 
+}
